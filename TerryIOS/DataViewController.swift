764938da60569aa42ba00bd274b6facf9fb5a017
Terry918 <terry800918@gmail.com>
2016-09-10 20:33:48 +0800

    aaa

---
+++ TerryIOS/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  TerryIOS
+//
+//  Created by 夏晖 on 9/10/16.
+//  Copyright © 2016 夏晖. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
